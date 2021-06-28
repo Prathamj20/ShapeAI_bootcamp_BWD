9a3f2950b548a0e2fcac2d100367348f9426d692
neha016 <54343462+neha016@users.noreply.github.com>
2021-06-28 13:41:26 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JAVASCRIPT AND REACTJS BOOTCAMP</h1>
+      <p>This is a basic javascripta nad React.js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
